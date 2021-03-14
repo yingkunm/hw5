7af245d7c6ec2b1b0abb434e4a3864b14df1fdc2
yingkunm <yingkunm@umich.edu>
2021-03-14 16:36:56 +0800

    hw5

--- hw5_tests.py
+++ hw5_tests.py
@@ -27,7 +27,7 @@ class TestCard(unittest.TestCase):
         self.assertEqual(c2.rank, 1)
         self.assertEqual(c2.rank_name, "Ace")
         
-    def test_q1_construct_Card(self):
+    def test_q1(self):
         '''
         1. fill in your test method for question 1:
         Test that if you create a card with rank 12, its rank_name will be "Queen"
@@ -40,11 +40,12 @@ class TestCard(unittest.TestCase):
 
         '''
         c = hw5_cards.Card(0, 12)
-        X = self.assertEqual(c.rank, 12)
-        Y = self.assertEqual(c.rank_name, "Queen")
+        X = Y = c.rank_name
+        self.assertEqual(c.rank, 12)
+        self.assertEqual(c.rank_name, "Queen")
         return X, Y
     
-    def test_q2_construct_Card(self):
+    def test_q2(self):
         '''
         1. fill in your test method for question 1:
         Test that if you create a card instance with suit 1, its suit_name will be "Clubs"
@@ -57,12 +58,13 @@ class TestCard(unittest.TestCase):
 
         '''
         c = hw5_cards.Card(1, 12)
-        X = self.assertEqual(c.suit, 1)
-        Y = self.assertEqual(c.suit_name, "Clubs")
+        X = Y = c.suit_name
+        self.assertEqual(c.suit, 1)
+        self.assertEqual(c.suit_name, "Clubs")
         return X, Y   
     
 
-    def test_q3___str__method(self):
+    def test_q3(self):
         '''
         1. fill in your test method for question 3:
         Test that if you invoke the __str__ method of a card instance that is created with suit=3, rank=13, it returns the string "King of Spades"
@@ -76,10 +78,11 @@ class TestCard(unittest.TestCase):
 
         '''
         c = hw5_cards.Card(3,13)
-        X = self.assertEqual(c.__str__(), "King of Spades")
-        return X
+        X = Y = c.__str__()
+        self.assertEqual(c.__str__(), "King of Spades")
+        return X, Y
     
-    def test_q4_create_deck(self):
+    def test_q4(self):
         '''
         1. fill in your test method for question 4:
         Test that if you create a eck instance, it will have 52 cards in its cards instance variable
@@ -92,10 +95,11 @@ class TestCard(unittest.TestCase):
 
         '''
         deck = hw5_cards.Deck()
-        X = self.assertEqual(len(deck.cards),52)
-        return X
+        X = Y = len(deck.cards)
+        self.assertEqual(len(deck.cards),52)
+        return X, Y
 
-    def test_q5_deal_card_method(self):
+    def test_q5(self):
         '''
         1. fill in your test method for question 5:
         Test that if you invoke the deal_card method on a deck, it will return a card instance.
@@ -109,10 +113,11 @@ class TestCard(unittest.TestCase):
         '''
         deck = hw5_cards.Deck()
         c = deck.deal_card()   
-        X = self.assertIsInstance(c, hw5_cards.Card)
-        return X
+        X = Y = c
+        self.assertIsInstance(c, hw5_cards.Card)
+        return X, Y
     
-    def test_q6_deal_card_method(self):
+    def test_q6(self):
         '''
         1. fill in your test method for question 6:
         
@@ -127,11 +132,12 @@ class TestCard(unittest.TestCase):
         '''
         deck = hw5_cards.Deck()
         c = deck.deal_card()   
-        X = self.assertEqual(len(deck.cards),51)
-        return X   
+        X = Y = len(deck.cards)
+        self.assertEqual(len(deck.cards),51)
+        return X, Y
     
 
-    def test_q7_replace_card_method(self):
+    def test_q7(self):
         '''
         1. fill in your test method for question 7:
         Test that if you invoke the replace_card method, the deck has one more card in it afterwards. (Please note that you want to use deal_card function first to remove a card from the deck and then add the same card back in)
@@ -145,14 +151,17 @@ class TestCard(unittest.TestCase):
 
         '''
         deck = hw5_cards.Deck()
+        X = len(deck.cards)
         c1 = deck.deal_card()
         l1 = len(deck.cards)
+        Y = l1 +1
         deck.replace_card(c1)
         l2 = len(deck.cards)
-        X = self.assertEqual(l1 + 1, l2)
-        return X
+        Z = l2
+        self.assertEqual(l1 + 1, l2)
+        return X, Y, Z
     
-    def test_q8_replace_card_method(self):
+    def test_q8(self):
         '''
         1. fill in your test method for question 8:
         Test that if you invoke the replace_card method with a card that is already in the deck, the deck size is not affected.(The function must silently ignore it if you try to add a card that’s already in the deck)
@@ -168,10 +177,12 @@ class TestCard(unittest.TestCase):
         deck = hw5_cards.Deck()
         for i in deck.cards:
             l1 = len(deck.cards)
+            X = l1
             deck.replace_card(i)
             l2 = len(deck.cards)
-            X = self.assertEqual(l1, l2)
-        return X 
+            Y = l2
+            self.assertEqual(l1, l2)
+        return X, Y
 
 
 
